02a2e70270954a6c6221fa3776bcdeebfdd96bd7
kaiojoay15 <149083541+kaiojoay15@users.noreply.github.com>
2024-09-26 17:01:51 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'O que são fontes hidrotermais?',
+    'São fissuras no fundo do mar que liberam água quente rica em minerais.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos metros de profundidade a fossa das Marianas tem?',
+    '11 mil metros de profundidade'
+)
+
+criaCartao(
+    'Geografia',
+    'Existe vida nas fossas das Marianas?',
+    'Sim existe, alguns exemplos são foraminíferos, bactérias, diatomáceas e radiolários, micróbios, vírus bacteriófago.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é a zona abissal?',
+    'É a parte mais profunda e escura do oceano, abaixo de 4.000 metros'
+)
